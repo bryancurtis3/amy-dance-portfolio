19a3b1efd056f579c9b20167e685ea1b2c7723fd
bryancurtis <curtispro97@gmail.com>
2022-08-17 19:50:03 -0400

    remembered to make the gitignore and secure the secret key before push

--- amy_project/settings.py
+++ amy_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+# python_dotenv for .env variable access
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6r)faw@qw4j0m-bv7%i1!*9bxm^)v=1v4tcqiraln-teb*x#bc'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
