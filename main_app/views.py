b58b75715425dbee75efdaf8e8d7fb85711837a3
bryancurtis <curtispro97@gmail.com>
2022-08-17 19:42:35 -0400

    initial commit, ported static page to django server, need to sort extensions next

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+# Here we will be creating a class called Home and extending it from the View class
+class Home(TemplateView):
+    template_name = "home.html"
